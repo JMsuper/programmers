0ad6bb39b87a90ddee1d1921038b845fcae81778
JMsuper <tjsqls2067@naver.com>
2024-01-18 16:19:19 +0900

    fix 무한 루프에 조심하자

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -20,17 +20,7 @@ class Solution {
         ArrayList<int[]> diceCombiList = new ArrayList<>();
     
         combi(iterArr, visited,diceCombiList,0,m,m);
-        // diceCombiList.forEach(i -> print(i));
-        
-        
-        // 각 조합에 대한 승 횟수 계산
-        // (1) HashMap : Key = 조합의 경우, Value = 승 횟수
-        // - 함수 호출 조건 : 나의 조합이 승 횟수를 담은 맵에 없어야 함 
-        //   -> 있다면, 다른 조합을 통해 이미 도출된것
-        // (2) 나의 조합과 상대 조합을 함수에 넘겨줌
-        //   - 승리 횟수 반환
-        //   -> HashMap에 나의 조합에 해당하는 Key에 승 횟수 입력
-        
+
         // (1) 승리 횟수를 담는 맵 생성
         HashMap<int[], Integer> winCntMap = new HashMap<>();
         
@@ -53,15 +43,14 @@ class Solution {
                 otherDiceArr[cur] = i;
                 cur += 1;
             }
-            // print(myDiceList);
-            // print(otherDiceList);
+
             Integer winCnt = getWinCnt(dice, myDiceArr, otherDiceArr);
 
             winCntMap.put(myDiceArr, winCnt);
         }
 
         int maxWinCnt = 0;
-        // System.out.println(winCntMap);
+
         for(int[] key : winCntMap.keySet()){
             if(winCntMap.get(key) > maxWinCnt){
                 maxWinCnt = winCntMap.get(key);
@@ -91,14 +80,6 @@ class Solution {
             }
         }
     }
-
-    void print(int[] arr){
-        for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-    
         
     public int getWinCnt(int[][] dice, int[] myDiceArr, int[] otherDiceArr){
         int result = 0;
@@ -114,17 +95,10 @@ class Solution {
         
         int curIdx = otherSumList.size() / 2;
         for(int mySum : mySumList){
-            
-            // System.out.println(mySum);
-            // System.out.println(otherSumList);
 
             int[] winCntAndIdx = binarySearch(otherSumList, mySum, 0, otherSumList.size(), curIdx,1);
             result += winCntAndIdx[0];
             curIdx = winCntAndIdx[1];
-            // System.out.println(winCntAndIdx[0]);
-            // System.out.println(curIdx);
-            // System.out.println(result);
-            // System.out.println();
         }
         
         return result;
@@ -144,21 +118,12 @@ class Solution {
     
     // target 보다 작지만, 그 다음 인덱스는 target 보다 큰 인덱스를 찾는, 이진 탐색
     public static int[] binarySearch(ArrayList<Integer> otherSumList, int target, int lower, int upper, int curIdx, int cnt){
-        if(cnt > 50){
-            System.out.println(target + " " +  lower + " " +   upper + " " +   curIdx + " " +   cnt);
-            System.out.println("HERE~");
-            return new int[]{-1,-1};
-            
-        }
         if(curIdx >= otherSumList.size() || otherSumList.get(otherSumList.size() - 1) < target){
             return new int[]{ otherSumList.size() , curIdx};
         }
         if(curIdx < 0 || otherSumList.get(0) >= target){
             return new int[]{ 0, curIdx};
         }
-
-        // val >= target -> curIdx = curIdx / 2
-        // val < target -> curIdx = curIdx + ( arr.length - curIdx ) / 2
 
         int val = otherSumList.get(curIdx);
 
@@ -176,6 +141,5 @@ class Solution {
 
         return new int[]{-1,-1};
     }
-    
-    
+
 }
